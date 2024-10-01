094d896476f4ed368273b706950e049de0e32f51
morgashen <mashudumorgan@gmail.com>
2024-10-01 19:16:25 +0200

    0x00-ES6_basic

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,3 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  return array.map((value) => `${appendString}${value}`);
+  const newArray = [];
+  for (let idx = 0; idx < array.length; idx += 1) {
+    const value = array[idx];
+    newArray.push(appendString + value);
+  }
+  return newArray;
 }

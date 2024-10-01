24042e0948c6c05429d30a48fac5c16e7087c43f
morgashen <mashudumorgan@gmail.com>
2024-10-01 20:39:53 +0200

    0x01-ES6_promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,12 @@
 export default function getResponseFromAPI() {
+
   return new Promise((resolve, reject) => {
-    // Simulating an API call
     setTimeout(() => {
-      const success = true; // You can change this to false to simulate a failed API call
-      if (success) {
-        resolve("API response data");
-      } else {
-        reject(new Error("API call failed"));
-      }
-    }, 1000); // Simulating a delay of 1 second
+      const data = {
+        message: "Success",
+        status: 200,
+      };
+      resolve(data);
+    }, 2000);
   });
 }
